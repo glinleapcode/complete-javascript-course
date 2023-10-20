0dece1c8b26ab595d35c53ee9c7f18647127c887
Brenda <ziruhe@gmail.com>
2023-10-20 23:26:01 +0000

    Oct 20, 2023, 4:26 PM

--- 05-Guess-My-Number/10-20-23/script.js
+++ 05-Guess-My-Number/10-20-23/script.js
@@ -23,10 +23,22 @@ document.querySelector('.check').addEventListener('click', e => {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct Number!';
   } else if (guess < secretNumber) {
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low!';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game!';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
