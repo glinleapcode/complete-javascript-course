e2e36629348beaeaa7700c2c810722ba182c3f64
Brenda <ziruhe@gmail.com>
2023-10-20 21:03:37 +0000

    Oct 20, 2023, 2:03 PM

--- 05-Guess-My-Number/10-20-23/script.js
+++ 05-Guess-My-Number/10-20-23/script.js
@@ -1,3 +1,6 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '👏 Correct Number!';
+document.querySelector('.btn.check').addEventListener('click', e => {
+  console.log('button clicked');
+});
