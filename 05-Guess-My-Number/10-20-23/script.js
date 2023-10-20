2e9c5bdbd70ee8b0f069ec63b65adf9a2ab871c0
Brenda <ziruhe@gmail.com>
2023-10-20 23:20:44 +0000

    Oct 20, 2023, 4:20 PM

--- 05-Guess-My-Number/10-20-23/script.js
+++ 05-Guess-My-Number/10-20-23/script.js
@@ -13,13 +13,15 @@ document.querySelector('.score').textContent = 89;
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 let score = 20;
-let message = document.querySelector('.message').textContent;
 
 document.querySelector('.check').addEventListener('click', e => {
   let guess = Number(document.querySelector('.guess').value);
 
   if (guess === '' || isNaN(guess) || guess <= 0 || guess > 20) {
-    message = '🏀 Invalid Number, Please enter a number between 1-20';
+    document.querySelector('.message').textContent =
+      '🏀 Invalid Number, must between 1-20';
+    console.log(document.querySelector('.message').textContent);
+    // console.log(message);
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct Number!';
   } else if (guess < secretNumber) {
