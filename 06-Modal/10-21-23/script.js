de2c300ca95713f42b742712ad2bb815c249f2bd
Brenda <ziruhe@gmail.com>
2023-10-22 05:32:42 +0000

    Oct 21, 2023, 10:32 PM

--- 06-Modal/10-21-23/script.js
+++ 06-Modal/10-21-23/script.js
@@ -4,4 +4,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+btnsOpenModal.forEach((value, index) => {
+  console.log(value + index);
+});
