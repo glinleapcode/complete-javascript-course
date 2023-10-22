a31c5730a5c20c15ff72cf7b956e6e30d4a52d23
Brenda <ziruhe@gmail.com>
2023-10-22 15:08:13 +0000

    Oct 22, 2023, 8:08 AM

--- 04-HTML-CSS/10-22-23/script.js
+++ 04-HTML-CSS/10-22-23/script.js
@@ -1,14 +1,2 @@
-<html>
-  <head>
-    <title>Learning HTML & CSS</title>
-  </head>
-  <body>
-    <h1> JavaScript is fun, but so it is HTML & CSS</h1>
-    <p>
-      you can learn JavaScript without HTML and CSS, but for DOM manipulation
-      you need to know HTML and CSS, And it is even more fun when you can use
-      all three together
-    </p>
-    <p>Just another paragraph</p>
-  </body>
-</html>;
+
+
