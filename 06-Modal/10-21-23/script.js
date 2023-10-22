a5f40da7c7e25b6b4b68d67c3bb491439acbc96f
Brenda <ziruhe@gmail.com>
2023-10-22 06:16:21 +0000

    Oct 21, 2023, 11:16 PM

--- 06-Modal/10-21-23/script.js
+++ 06-Modal/10-21-23/script.js
@@ -4,18 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-btnsOpenModal.forEach(btn => {
-  btn.addEventListener('click', () => {
-    console.log(btn.textContent + ' button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-});
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 
