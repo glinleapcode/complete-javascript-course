b25ec2e6d2dd6d2a897d69c527ec4dbfe2d17976
Brenda <ziruhe@gmail.com>
2023-10-26 17:07:04 +0000

    Oct 26, 2023, 10:07 AM

--- 13-Advanced-DOM-Bankist/10-23-23/script.js
+++ 13-Advanced-DOM-Bankist/10-23-23/script.js
@@ -84,7 +84,6 @@ tabsContainer.addEventListener('click', function (e) {
   // console.log(e.target.closest('.btn'));
   const clickedTab = e.target.closest('.operations__tab');
   if (!clickedTab) return; // if click on the empty space between the buttons
-  console.log(clickedTab.parentNode);
 
   clickedTab.classList.add('operations__tab--active');
 });
