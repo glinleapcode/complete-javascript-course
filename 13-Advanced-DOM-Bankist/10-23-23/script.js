0277f0f92fa9233f0db1909c716ee7b711d0e35b
Brenda <ziruhe@gmail.com>
2023-10-26 00:36:53 +0000

    Oct 25, 2023, 5:36 PM

--- 13-Advanced-DOM-Bankist/10-23-23/script.js
+++ 13-Advanced-DOM-Bankist/10-23-23/script.js
@@ -77,7 +77,7 @@ console.log(h1.nextElementSibling);
 // Tabbed Component Operations
 
 const tabs = document.querySelectorAll('.operations_tab');
-const tabContainer = document.querySelector('.operations_tab-container');
-tabContainer.addEventListener('click', function (e) {
+const tabsContainer = document.querySelector('.operations_tab-container');
+tabsContainer.addEventListener('click', function (e) {
   console.log(e.target);
 });
