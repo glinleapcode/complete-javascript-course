68c7631eddb39cf1acbecc10726ae17f229b5a78
Brenda <ziruhe@gmail.com>
2023-10-26 21:24:36 +0000

    Oct 26, 2023, 2:24 PM

--- 13-Advanced-DOM-Bankist/10-23-23/script.js
+++ 13-Advanced-DOM-Bankist/10-23-23/script.js
@@ -114,6 +114,5 @@ const nav = document.querySelector('.nav');
 const logo = document.querySelector('.nav__logo');
 
 nav.addEventListener('mouseover', function (e) {
-  const li = e.target;
-  console.log(li.closest('nav__item'));
+  console.log(e.target.closest('.nav__item'));
 });
