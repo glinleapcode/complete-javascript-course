d700ea02712c8c784589c6cb9142ddaca9bd8d26
Brenda <ziruhe@gmail.com>
2023-10-27 04:03:42 +0000

    Oct 26, 2023, 9:03 PM

--- 13-Advanced-DOM-Bankist/10-23-23/script.js
+++ 13-Advanced-DOM-Bankist/10-23-23/script.js
@@ -188,8 +188,9 @@ nav.addEventListener('mouseout', handlehoover.bind(1));
 // Sticky navigation
 
 const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
 window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
+  //console.log(window.scrollY);
 
   if (window.scrollY > initialCoords.top) {
     nav.classList.add('sticky');
