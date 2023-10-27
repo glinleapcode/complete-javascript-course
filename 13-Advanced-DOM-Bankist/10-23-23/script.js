3a155e7e9dc2ec951d7a0f80a6b11187dcfa3d8c
Brenda <ziruhe@gmail.com>
2023-10-27 03:58:54 +0000

    Oct 26, 2023, 8:58 PM

--- 13-Advanced-DOM-Bankist/10-23-23/script.js
+++ 13-Advanced-DOM-Bankist/10-23-23/script.js
@@ -191,7 +191,7 @@ const initialCoords = section1.getBoundingClientRect();
 window.addEventListener('scroll', function () {
   console.log(window.scrollY);
 
-  if (this.window.scrollY > initialCoords.top) {
+  if (window.scrollY > initialCoords.top) {
     nav.classList.add('sticky');
   } else {
     nav.classList.remove('sticky');
