dba8025eff697bee0ff23500b79231d9f2e3c91e
Brenda <ziruhe@gmail.com>
2023-10-28 23:47:31 +0000

    Oct 28, 2023, 4:47 PM

--- 07-Pig-Game/10-22-23/script.js
+++ 07-Pig-Game/10-22-23/script.js
@@ -29,6 +29,7 @@ btnRoll.addEventListener('click', function (e) {
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
     } else {
+      //switch player
       document.getElementById(`current--${activePlayer}`).textContent = 0;
       currentScore = 0;
       activePlayer = activePlayer === 0 ? 1 : 0;
@@ -51,6 +52,8 @@ btnHold.addEventListener('click', function () {
       //switch player
       currentScore = 0;
       activePlayer = activePlayer === 0 ? 1 : 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player-active');
     }
   }
 });
