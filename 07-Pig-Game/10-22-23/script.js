e25a9e397c183afecd06afc0005ecd69e345348c
Brenda <ziruhe@gmail.com>
2023-10-28 04:03:36 +0000

    Oct 27, 2023, 9:03 PM

--- 07-Pig-Game/10-22-23/script.js
+++ 07-Pig-Game/10-22-23/script.js
@@ -1,9 +1,9 @@
+const score0El = document.querySelector('#score--1');
+const score1El = document.querySelector('#score--2');
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
-const score0El = document.querySelector('#score--1');
-const score1El = document.querySelector('#score--2');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
